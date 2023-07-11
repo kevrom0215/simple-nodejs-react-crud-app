5c9efab55508cb2a574d47f93fad693537ff67af
Kenneth Araga <kenneth.araga@pointwest.com.ph>
2023-07-11 14:43:01 +0800

    changed login middleware implementation

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const login = require("../middleware/loginAuthenticator")
+const login = require("../services/loginService")
 
 
 
