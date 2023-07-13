e271e8ebddbb719a9c21bd7fbf3660fabeea57b5
Kenneth Araga <kenneth.araga@pointwest.com.ph>
2023-07-13 22:27:14 +0800

    fixed login route file

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const login = require("../services/loginService")
+const {authenticate} = require("../services/loginService")
 
 
 
@@ -10,29 +10,11 @@ router.get("/", function(req,res){
 
 
 router.post("/", async (req,res,next)=>{
-    if(!req || !req.headers.authorization){
-        res.status(401).send(`<html>
-        <body align=center>        
-                    <img height=100% src='https://http.cat/401.jpg'/>
-                    </body>
-                </html>`)
+    try{
+        await authenticate(req,res)
     }
-    else{
-        let base64Encoding = req.headers.authorization.split(" ")[1];
-        let credentials = Buffer.from(base64Encoding, "base64").toString().split(":");
-        const email = credentials[0];
-        const password = credentials[1];
-        const isAuthenticated = await login.authenticateUser(email, password)
-        if(isAuthenticated.length != 0){
-            res.status(200).send({
-                "message": "user authenticated"
-            })
-        }
-        else{
-            res.status(400).send({
-                "message": "no match"
-            })
-        }
+    catch(e){
+        console.error(e)
     }
 })
 
--- backend/services/loginService.js
+++ backend/services/loginService.js
@@ -11,6 +11,39 @@ const authenticateUser = async function(username, password) {
   });
 };
 
+
+const reqHeaderChecker = async (req) =>{
+  if(!req || !req.headers.authorization){
+    return false;
+  }
+  else{
+    return true;
+  }
+}
+
+const authenticate = async function(req,res){
+  if(reqHeaderChecker(req)){
+    let base64Encoding = req.headers.authorization.split(" ")[1];
+    let credentials = Buffer.from(base64Encoding, "base64").toString().split(":");
+    const email = credentials[0];
+    const password = credentials[1];
+    const isAuthenticated = await authenticateUser(email,password)
+    if(isAuthenticated.length != 0){
+      res.status(200).send({
+        "message": "user authenticated"
+      })
+    }
+    else{
+      res.status(400).send({
+        "message": "no match"
+    })
+    }
+  }
+  else{
+    res.status(400).send("Invalid Request")
+  }
+}
+
 module.exports = {
-    authenticateUser
+    authenticate
 }
